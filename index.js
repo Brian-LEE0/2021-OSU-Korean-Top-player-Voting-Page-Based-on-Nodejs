f8914333a96c5bef490dbb5a8500ae63811aa6ec
NNAisle <liy908@naver.com>
2021-12-09 04:49:35 +0900

    2112090449

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const bodyParser = require("body-parser")
 const { cookie } = require("request")
 
 const app = express()
-//const url = "http://118.91.37.158:80"
+    //const url = "http://118.91.37.158:80"
 const url = "http://localhost:80"
 
 const sql_info = {
-    host     : 'localhost',
-    user     : 'root',
-    password : 'nnaisle123',
-    database : '2021_OsuKoreanTopPlayer'
+    host: 'localhost',
+    user: 'root',
+    password: '485480',
+    database: '2021_OsuKoreanTopPlayer'
 }
 var con = mysql.createConnection(sql_info);
 const compare = {
-    YEAR : 2021,
-    MONTH : 05,
-    playcount : 5000
+    YEAR: 2021,
+    MONTH: 05,
+    playcount: 5000
 }
 
 const vote_info = [
@@ -33,24 +33,39 @@ const vote_info = [
     [1, 1, 1],
     [1, 1, 1],
     [1, 1, 1],
-]
+];
 
-con.connect((err)=>{
-    if(err) throw err
+con.connect((err) => {
+    var temp;
+    if (err) console.log(err)
     console.log('Connected DB')
-    try{
-        con.query('CREATE DATABASE '+sql_info.database,(err,result)=>{
-            if(err){
-                
-            }else{
-                console.log('database created')
-            }
-            
-        })
-    }catch(err){
-        console.log(err);
-    }
+    con.query('CREATE TABLE voter (id int not null primary key)', (err, result) => {
+        if (!err) console.log('success to make TABLE voter')
+    });
+    AddRow('voter', "username VARCHAR(40) not null")
+    vote_info.forEach((i, i_index) => i.forEach((j, j_index) => {
+        if (j) {
+            temp = String(10 - i_index) + "_" + String(1 + j_index) + " VARCHAR(40)"
+            if (j_index == 0) temp += " NOT NULL"
+            AddRow('voter', temp)
+            temp = String(10 - i_index) + "_" + String(1 + j_index) + "_avatar" + " VARCHAR(400)"
+            if (j_index == 0) temp += " NOT NULL"
+            AddRow('voter', temp)
+            temp = String(10 - i_index) + "_" + String(1 + j_index) + "_id" + " VARCHAR(30)"
+            if (j_index == 0) temp += " NOT NULL"
+            AddRow('voter', temp)
+        }
+    }))
 });
+
+function AddRow(table, str) {
+    var temp;
+    temp = 'ALTER TABLE ' + table + ' ADD '
+    temp += str;
+    con.query(temp, (err, result) => {
+        if (!err) console.log(temp, 'success')
+    })
+}
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -59,12 +74,12 @@ app.use(express.static("assets")) // path of assets
 app.set("view engine", "ejs")
 
 app.listen(80, () => {
-    console.log("Server is running on port : 80\nurl : " + url)
-})
+    console.log("Server is running on port : 80\nurl : " + url);
+});
 
 app.get("/", (req, res) => {
-    return res.render("index")
-})
+    return res.render("index");
+});
 
 app.get("/login", (req, res) => {
     const uri =
@@ -73,13 +88,13 @@ app.get("/login", (req, res) => {
         "redirect_uri=" +
         url +
         "/authorize&" +
-        "response_type=code&scope=public"
-    return res.redirect(uri)
-})
+        "response_type=code&scope=public";
+    return res.redirect(uri);
+});
 
 
-app.get("/authorize", async (req, res) => {
-    const { code } = req.query
+app.get("/authorize", async(req, res) => {
+    var { code } = req.query
     const result = await request_prom({
         method: "POST",
         url: "https://osu.ppy.sh/oauth/token",
@@ -95,16 +110,16 @@ app.get("/authorize", async (req, res) => {
             redirect_uri: "http://localhost:80/authorize",
         },
         json: true,
-    })
-    res.cookie("Accept", "application/json")
-    res.cookie("Content-Type", "application/json")
-    res.cookie("Authorization", "Bearer " + result["access_token"])
+    });
+    res.cookie("Accept", "application/json");
+    res.cookie("Content-Type", "application/json");
+    res.cookie("Authorization", "Bearer " + result["access_token"]);
 
-    res.redirect("/main")
-})
+    res.redirect("/main");
+});
 
 app.get("/main", (req, res) => {
-    try{
+    try {
         if (!req.cookies["Authorization"]) {
             res.redirect("/")
         }
@@ -118,16 +133,17 @@ app.get("/main", (req, res) => {
             method: "GET",
             headers: headers,
         }
-        request(options, function (error, response, body) {
+        request(options, function(error, response, body) {
             const obj = JSON.parse(body)
             const join_date = obj.join_date.split('T')[0].split('-')
-            if(obj.statistics.play_count < compare.playcount){
+            if (obj.statistics.play_count < compare.playcount) {
                 res.send("<script>alert('플레이 카운트가 5000 미만입니다. 투표에 참여하실 수 없습니다.')location.href='/'</script>")
-            }else if(Number(join_date[0])>=compare.YEAR || Number(join_date[1]) >= compare.MONTH){
-                res.send("<script>alert('최초가입일이 "  + compare.YEAR + "년 " +compare.MONTH + "월 이후입니다. 투표에 참여하실 수 없습니다.')location.href='/'</script>")
-            }else{
+            } else if (Number(join_date[0]) >= compare.YEAR || Number(join_date[1]) >= compare.MONTH) {
+                res.send("<script>alert('최초가입일이 " + compare.YEAR + "년 " + compare.MONTH + "월 이후입니다. 투표에 참여하실 수 없습니다.')location.href='/'</script>")
+            } else {
                 const params = {
                     nicname_me: obj["username"],
+                    id_me: obj["id"],
                     image_me: obj["avatar_url"],
                     userpage_me: "osu.ppy.sh/users/" + obj["id"],
                     vote_info: vote_info,
@@ -136,7 +152,7 @@ app.get("/main", (req, res) => {
                 res.render("voting.ejs", params)
             }
         })
-    }catch(err){
+    } catch (err) {
         res.redirect("/")
     }
 })
@@ -146,60 +162,119 @@ app.post("/main", (req, res) => {
         Accept: req.cookies["Accept"],
         "Content-Type": req.cookies["Content-Type"],
         Authorization: req.cookies["Authorization"],
-    }
+    };
     if (!req.body) {
         return res.status(400).json({
             status: "error",
             error: "req body cannot be empty",
-        })
+        });
     }
     val = req.body["name"]
+    inputname = req.body["inputname"]
+    console.log(req.body);
     request({
-        url: "https://osu.ppy.sh/api/v2/users/" + val,
-        //url: 'https://osu.ppy.sh/api/v2/users/' + val + '/osu',
-        qs: {
-            mode: "osu",
-            limit: 1,
+            url: "https://osu.ppy.sh/api/v2/users/" + val,
+            //url: 'https://osu.ppy.sh/api/v2/users/' + val + '/osu',
+            qs: {
+                mode: "osu",
+                limit: 1,
+            },
+            method: "GET",
+            headers: headers,
         },
-        method: "GET",
-        headers: headers,
-    },
-        function (error2, response2, body2) {
+        function(error2, response2, body2) {
             var json = {}
             var Jbody = JSON.parse(body2)
-            try{
-                if (Jbody["username"] == 'undefined' || Jbody["country"]["code"] !='KR') {
+            try {
+                if (Jbody["username"] == 'undefined' || Jbody["country"]["code"] != 'KR') {
                     res.json({
                         name: "",
                         img: "",
                         rank: "",
+                        id: "",
+                        inputname: inputname
                     })
                     return 0
-                }else{
+                } else {
+
                     json.name = Jbody["username"]
                     json.img = Jbody["avatar_url"]
                     json.rank = Jbody["statistics"]["global_rank"]
+                    json.id = Jbody["id"]
+                    json.inputname = inputname;
                     console.log(json)
                     json.rank = "# " + json.rank
                     res.json(json)
-                    return 0 
+                    return 0
                 }
 
-            }catch(err){
+            } catch (err) {
                 res.json({
                     name: "",
                     img: "",
                     rank: "",
+                    id: "",
+                    inputname: inputname
                 })
             }
-            
-            
+
+
         })
 })
 
-app.get("/submit",(req,res) =>{
-    var submitted_data = JSON.parse(req.body);
-    
-    con.query("")
+app.post("/submit", (req, res) => {
+    var submitted_data = req.body;
+    var data = submit_list(req.body)
+    if (data == 1) {
+        res.send("<script>alert('투표에 참여해주셔서 감사합니다!\nThank You for Voting!')location.href='/'</script>")
+    } else {
+        res.send("<script>alert('SysError!!!\n관리자에게 문의해주십시오!')</script>")
+    }
 
 })
+
+function retrieveDB(username) {
+    con.query("SELECT * FROM voter WHERE username = '" + username + "'", (err, result) => {
+        console.log(err)
+        console.log("go", result)
+    });
+}
+
+
+function submit_list(json) {
+    var temp = "INSERT INTO voter (username, id, "
+    vote_info.forEach((i, i_index) => i.forEach((j, j_index) => {
+        if (j) {
+            temp += String(10 - i_index) + "_" + String(1 + j_index) + ", "
+            temp += String(10 - i_index) + "_" + String(1 + j_index) + "_avatar, "
+            temp += String(10 - i_index) + "_" + String(1 + j_index) + "_id"
+            if (i_index != vote_info.length - 1 || j_index != i.length - 1) temp += ", "
+        }
+    }))
+    temp += ") VALUES ("
+    temp += "'" + json.username + "','" + json.id + "',";
+    vote_info.forEach((i, i_index) => i.forEach((j, j_index) => {
+        if (j) {
+            if (json[String(10 - i_index) + "_" + String(1 + j_index)]) {
+                temp += "'" + json[String(10 - i_index) + "_" + String(1 + j_index)] + "',"
+                temp += "'" + json[String(10 - i_index) + "_" + String(1 + j_index) + "_avatar"] + "',"
+                temp += "'" + json[String(10 - i_index) + "_" + String(1 + j_index) + "_id"] + "'"
+
+
+            } else {
+                temp += "NULL, NULL, NULL"
+
+            }
+            if (i_index != vote_info.length - 1 || j_index != i.length - 1) temp += ", "
+
+        }
+    }))
+    temp += ");"
+    con.query(temp, (err, result) => {
+        if (err)
+            con.query("DELETE FROM voter WHERE id = '" + json.id + "'", (err, result) => {
+                if (!err) submit_list(json)
+            })
+    })
+    return 1;
+}
